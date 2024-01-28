65b43b7a855e7788e87d9bc7e4bedf56aff920b0
dockercompose-man <122501283+dockercompose-man@users.noreply.github.com>
2024-01-28 14:59:31 -0500

    FGHGFHFG

--- EULA-WORKING.py
+++ EULA-WORKING.py
@@ -41,6 +41,24 @@ def show_eula():
     eula_window.protocol("WM_DELETE_WINDOW", agree)
 
 
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("PDF files", "*.pdf")])
+    if file:
+        read_pdf = pypdf.PdfReader(file)
+        page = read_pdf._get_page(0)
+        page_content = page.extract_text()
+        print(page_content)
+
+        #Text Box
+        text_box = tk.Text(root,  height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+
+        browse_text.set("Browse")
+
 # Initialize Tkinter
 root = tk.Tk()
 
